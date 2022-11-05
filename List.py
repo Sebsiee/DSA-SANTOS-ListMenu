e1ca8cc67b0653bb3ee378c0ee468a630443ff99
unknown <santosbasty4@gmail.com>
2022-11-05 12:00:18 +0800

    First Commit - Menu

---
+++ List.py
@@ -0,0 +1,16 @@
+def display():
+    list1 = [1, 4, 3, 4, 4, 5, 6, 2, 56, 200]
+    print("Menu:")
+    print("1 -> Add an element")
+    print("2 -> Insert an element")
+    print("3 -> Modify an element")
+    print("4 -> Delete an element")
+    print("5 -> Arrange in ascending order")
+    print("6 -> Arrage in descending order")
+    print("7 -> Count elements")
+    print("8 -> Smallest element")
+    print("9 -> Largest element\n")
+    print(f"List: {list1}")
+    whatToDo = input("What do you want to do? (1-9): ")
+
+display()
